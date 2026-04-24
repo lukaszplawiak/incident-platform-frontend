14a48ff0e2a5d444bd33b8cdb9c5f0953da29c90
Łukasz Pławiak <lukaszplawiakzero1@gmail.com>
2026-04-25 00:55:17 +0200

    feat(config): add ApplicationConfig, lazy-loaded routes with authGuard, placeholder components for all routes

---
+++ src/app/features/incidents/incident-detail/incident-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incident-detail',
+  imports: [],
+  templateUrl: './incident-detail.html',
+  styleUrl: './incident-detail.scss',
+})
+export class IncidentDetail {}

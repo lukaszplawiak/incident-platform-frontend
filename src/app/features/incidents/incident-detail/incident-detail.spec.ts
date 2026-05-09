4bed89c2c747086675b37774bbc46f40d1962f82
Łukasz Pławiak <lukaszplawiakzero1@gmail.com>
2026-05-09 14:00:53 +0200

    refactor(incident-detail): migrate @Input to signal input

--- src/app/features/incidents/incident-detail/incident-detail.spec.ts
+++ src/app/features/incidents/incident-detail/incident-detail.spec.ts
@@ -1,11 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { ComponentRef } from '@angular/core';
 import { IncidentDetail } from './incident-detail';
 
 describe('IncidentDetail', () => {
   let component: IncidentDetail;
   let fixture: ComponentFixture<IncidentDetail>;
+  let componentRef: ComponentRef<IncidentDetail>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,6 +20,10 @@ describe('IncidentDetail', () => {
 
     fixture = TestBed.createComponent(IncidentDetail);
     component = fixture.componentInstance;
+    componentRef = fixture.componentRef;
+
+    componentRef.setInput('id', 'test-incident-id');
+
     await fixture.whenStable();
   });
 
